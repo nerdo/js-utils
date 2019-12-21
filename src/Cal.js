9438312d9c5fd97f35301df433319bfc1ad35b3b
Dannel Albert <cuebix@gmail.com>
2019-12-20 20:25:53 -0500

    - constructor

--- src/Cal.js
+++ src/Cal.js
@@ -1,16 +1,16 @@
-const defaults = {
-  constructor: {
-    DateAdapter: Date
-  }
-}
+// const defaults = {
+//   constructor: {
+//     DateAdapter: Date
+//   }
+// }
 
 export class Cal {
-  constructor(args = defaults.constructor) {
-    const {
-      DateAdapter = defaults.constructor.DateAdapter
-    } = args
-    this.DateAdapter = DateAdapter
-  }
+  // constructor(args = defaults.constructor) {
+  //   const {
+  //     DateAdapter = defaults.constructor.DateAdapter
+  //   } = args
+  //   this.DateAdapter = DateAdapter
+  // }
 }
 
 module.exports.Cal = Cal
--- src/Cal.test.js
+++ src/Cal.test.js
@@ -4,17 +4,4 @@ describe('Cal', () => {
   it('should be a function', () => {
     expect(Cal).toBeInstanceOf(Function)
   })
-
-  describe('constructor', () => {
-    it('should store DateAdapter', () => {
-      const MyDateAdapter = {}
-      const cal = new Cal({DateAdapter: MyDateAdapter})
-      expect(cal.DateAdapter).toBe(MyDateAdapter)
-    })
-
-    it('should use Date as the default DateAdapter', () => {
-      const cal = new Cal()
-      expect(cal.DateAdapter).toBe(Date)
-    })
-  })
 })
