555f12cab016c9b5b4679fa2a40bef35a673f518
Dannel Albert <cuebix@gmail.com>
2019-12-20 20:26:40 -0500

    ! Cal represent should be a function

--- src/Cal.test.js
+++ src/Cal.test.js
@@ -4,4 +4,11 @@ describe('Cal', () => {
   it('should be a function', () => {
     expect(Cal).toBeInstanceOf(Function)
   })
+
+  describe('represent', () => {
+    it('should be a function', () => {
+      const cal = new Cal()
+      expect(cal.represent).toBeInstanceOf(Function)
+    })
+  })
 })
