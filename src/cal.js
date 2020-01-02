310d4bcbc8d6a981ef87532b1910729f4fc1fb44
Dannel Albert <cuebix@gmail.com>
2020-01-02 16:11:36 -0500

    + preliminary represent months

--- src/cal.js
+++ src/cal.js
@@ -24,13 +24,30 @@ export const cal = {
       date = getDefault('represent.date')
     } = args
 
+    const parts = representUnit(args)
+
     return {
       unit,
-      date
+      date,
+      ...parts
     }
   }
 }
 cal.DateAdapter = Date
+
+const representUnit = function (args) {
+  return {
+    month: {
+      weeks: [
+        [null, null, null, 1, 2, 3, 4],
+        [5, 6, 7, 8, 9, 10, 11],
+        [12, 13, 14, 15, 16, 17, 18],
+        [19, 20, 21, 22, 23, 24, 25],
+        [26, 27, 28, 29, 30, 31, null]
+      ]
+    }
+  }
+}
 
 module.exports.cal = cal
 export default cal
--- src/cal.test.js
+++ src/cal.test.js
@@ -51,5 +51,27 @@ describe('cal', () => {
         })
       })
     })
+
+    describe('unit: month', () => {
+      describe('default start of week', () => {
+        it('should return the days in the month, starting with Sunday', () => {
+          const date = new Date(2020, 1, 1)
+          const rep = cal.represent({unit: 'month', date })
+          expect(rep.month).toBeInstanceOf(Object)
+          expect(rep.month.weeks).toBeInstanceOf(Array)
+          expect(rep.month.weeks.length).toBe(5)
+          expect(rep.month.weeks[0].length).toBe(7)
+          expect(rep.month.weeks[1].length).toBe(7)
+          expect(rep.month.weeks[2].length).toBe(7)
+          expect(rep.month.weeks[3].length).toBe(7)
+          expect(rep.month.weeks[4].length).toBe(7)
+          expect(rep.month.weeks[0]).toEqual([null, null, null, 1, 2, 3, 4])
+          expect(rep.month.weeks[1]).toEqual([5, 6, 7, 8, 9, 10, 11])
+          expect(rep.month.weeks[2]).toEqual([12, 13, 14, 15, 16, 17, 18])
+          expect(rep.month.weeks[3]).toEqual([19, 20, 21, 22, 23, 24, 25])
+          expect(rep.month.weeks[4]).toEqual([26, 27, 28, 29, 30, 31, null])
+        })
+      })
+    })
   })
 })
