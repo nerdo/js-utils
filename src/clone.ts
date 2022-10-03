a28b41bb36a985fd7e4833a56e4d6e75dac477f9
Dannel Albert <cuebix@gmail.com>
2022-10-03 19:20:23 -0400

    fix: clone objects with undefined values

--- src/clone.ts
+++ src/clone.ts
@@ -3,11 +3,14 @@ import map from './map'
 export const clone = <T>(subject: T): T =>
   map(subject, v => {
     const t = typeof v
-    if (t === 'number' || t === 'string' || t === 'boolean' || v === null) {
+
+    if (t === 'undefined' || t === 'number' || t === 'string' || t === 'boolean' || v === null) {
       return v
     }
+
     if (Array.isArray(v)) {
       return v.slice().map(clone)
     }
+
     return { ...(v as object) }
   })
